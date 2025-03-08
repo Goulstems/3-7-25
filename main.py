a9cf1bdce44beec999b24e19b9bfd08b1590e8dd
Josh Morvant <sk84life4evr@Gmail.com>
2025-03-07 19:11:31 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# GOAL: Utilize file.readline() to read a WHOLE file line-by line!
+
+# GOAL: (soon) SORT contents of fakefile alphabetically
+
+# ---
+
+# consider other variations of operations we can do on files!
+
+fileContents = open("fakefile.txt","r")
+# print(fileContents.read())
+print(fileContents.readline())
+print(fileContents.readline())
+fileContents.close()
